65186ccdb9e35fc7492d0609f619150e799c07bb
dmlesc <dmlurth@gmail.com>
2018-01-18 23:58:38 -0700

    flush started

---
+++ tcpdump_exporter/conf/env-local.js
@@ -0,0 +1,4 @@
+module.exports = {
+  elasticHost: 'http://fnsn0:9200',
+  flushFreq: 10000
+};
--- tcpdump_exporter/tcpdump_exporter.js
+++ tcpdump_exporter/tcpdump_exporter.js
@@ -1,14 +1,25 @@
 'use strict';
+process.on('uncaughtException', (err) => { console.log(err); });
 
+const conf = require('./conf/' + process.argv[2]);
+const elasticsearch = require('elasticsearch');
+const elastic = new elasticsearch.Client({
+  host: conf.elasticHost
+  //, log: 'trace'
+});
 var metrics = require('./metrics');
 metrics();
 //metrics.log();
 //setTimeout(metrics.log, 5000);
 
 const nic = 'ens3';
+var packetsQueue = [ [], [] ];
 
 const { spawn } = require('child_process');
 const tcpdump = spawn('tcpdump', ['-i', nic, '-nl']);
+
+tcpdump.stderr.on('data', (data) => { console.log(`stderr: ${data}`); });
+tcpdump.on('close', (code) => { console.log(`child process exited with code ${code}`); });
 
 tcpdump.stdout.on('data', (data) => {
   //console.log(`\n**************\nstdout:\n\n${data}\n**************\n`);
@@ -21,41 +32,39 @@ tcpdump.stdout.on('data', (data) => {
 
   for (var i=0; i<packets.length; i++) {
     //console.log(packets[i]);
-    var packet = packets[i];
+    var packet = packets[i].split(' ');
 
-    if (packet != '') {
-      var packet_split = packet.split(' ');
-      var ts = packet_split[0].split(':');
+    if (packet.length > 1) {
+      var ts = packet[0].split(':');
       var sec = ts[2].split('.');
       var timestamp = new Date(year, month, day, ts[0], ts[1], sec[0], sec[1] / 1000).toJSON();
-      //console.log(ts, timestamp)
 
-      if (packet_split[1] == 'IP') {
-        //console.log(packet);
+      if (packet[1] == 'IP') {
         var protocol = 'IP';
+        var src = parseIPPort(packet[2]);
+        var dst = parseIPPort(packet[4]);
+        var flags = packet[6].slice(1, -2);
+        var length = packet[packet.indexOf('length') + 1];
 
-        var src = parseIPPort(packet_split[2]);
-        var dst = parseIPPort(packet_split[4]);
-        //console.log(src, dst);
+        var doc = {};
+        doc.timestamp = timestamp;
+        doc.protocol = protocol;
+        doc.src_ip = src.ip;
+        doc.src_port = src.port;
+        doc.dst_ip = dst.ip;
+        doc.dst_port = dst.port;
+        doc.flags = flags;
+        doc.length = length;
 
-        var flags = packet_split[6].slice(1, -2);
-        //console.log(flags, packet_split[6]);
-
-        var length = packet_split[packet_split.indexOf('length') + 1];
-        //console.log(length);
-
-
+        packetsQueue[0].push(doc);
       }
-      else if (packet_split[1] == 'ARP,') {
+      else if (packet[1] == 'ARP,') {
         //console.log(packet);
       }
       else {
         //console.log(packet);
       }
     }
-    
-    
-
   }
 });
 
@@ -68,10 +77,44 @@ function parseIPPort(str) {
   return obj;
 }
 
-tcpdump.stderr.on('data', (data) => {
-  console.log(`stderr: ${data}`);
-});
 
-tcpdump.on('close', (code) => {
-  console.log(`child process exited with code ${code}`);
-});
+function flushPackets() {
+  console.log('flushPackets');
+
+  if (packetsQueue[0].length) {
+    var packets = packetsQueue.shift();
+    packetsQueue.push([]);
+    console.log('#packets: ', packets.length);
+
+    var bulk = [];
+
+    for (var i=0; i<packets.length; i++) {
+      var packet = packets[i];
+      var date = packet.timestamp.split('T')[0];
+      var index = {};
+      index._index = 'tcpdump-' + date;
+      index._type = 'doc';
+
+      var action = {};
+      action.index = index;
+      bulk.push(action);
+      bulk.push(packet);
+    }
+    console.log('bulk ready');
+    elasticBulk(bulk);
+  }
+}
+
+function elasticBulk(bulk) {
+  elastic.bulk({ body: bulk }, (err, res) => {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      //console.log(res);
+      console.log('bulk inserted');
+    }
+  });
+}
+
+var flushInterval = setInterval(flushPackets, conf.flushFreq);
