c39a7390c6731100c549f0d089255597bb3aac81
dmlesc <dmlurth@gmail.com>
2018-01-29 23:18:55 -0700

    multiple nics

--- tcpdump_exporter/tcpdump_exporter.js
+++ tcpdump_exporter/tcpdump_exporter.js
@@ -11,7 +11,7 @@ metrics();
 //metrics.log();
 //setTimeout(metrics.log, 5000);
 
-const nic = 'ens32';
+const NICs = ['ens32','ens33','ens34','ens35'];
 var packetsQueue = [ [], [] ];
 const interval = 5000;
 const packetsLimit = 5000;
@@ -19,49 +19,54 @@ var bulkQueue = [];
 var processing = false;
 
 const { spawn } = require('child_process');
-const tcpdump = spawn('tcpdump', ['-i', nic, '-l', '-s', '75']);
 
-tcpdump.stderr.on('data', (data) => { console.log(`stderr: ${data}`); });
-tcpdump.on('close', (code) => { console.log('close:', code); });
 
-tcpdump.stdout.on('data', (data) => {
-  var timestamp = new Date().toJSON();
-  var packets = data.toString().split('\n');
+function spawns(nic) {
+  const tcpdump = spawn('tcpdump', ['-i', nic, '-l', '-s', '75']);
 
-  for (var i=0; i<packets.length; i++) {
-    //console.log(packets[i]);
-    var packet = packets[i].split(' ');
-
-    if (packet.length > 1) {
-      if (packet[1] == 'IP') {
-        var src = parseHostPort(packet[2]);
-        var dst = parseHostPort(packet[4]);
-
-        var flags;
-        if (packet[6] == undefined) { flags = packet[5]; }
-        else { flags = packet[6].slice(1, -2); }
-
-        var doc = {};
-        doc.timestamp = timestamp;
-        doc.protocol = 'IP';
-        doc.src_host = src.host;
-        doc.src_port = src.port;
-        doc.dst_host = dst.host;
-        doc.dst_port = dst.port;
-        doc.flags = flags;
-        doc.length = packet[packet.indexOf('length') + 1];;
-
-        packetsQueue[0].push(doc);
-      }
-      else if (packet[1] == 'ARP,') { /*console.log(packet);*/ }
-      else { /*console.log(packet);*/ }
-
-      if (packetsQueue[0].length >= packetsLimit) {
-        flushPackets();
+  tcpdump.stderr.on('data', (data) => { console.log(`stderr: ${data}`); });
+  tcpdump.on('close', (code) => { console.log('close:', code); });
+  
+  tcpdump.stdout.on('data', (data) => {
+    var timestamp = new Date().toJSON();
+    var packets = data.toString().split('\n');
+  
+    for (var i=0; i<packets.length; i++) {
+      //console.log(packets[i]);
+      var packet = packets[i].split(' ');
+  
+      if (packet.length > 1) {
+        if (packet[1] == 'IP') {
+          var src = parseHostPort(packet[2]);
+          var dst = parseHostPort(packet[4]);
+  
+          var flags;
+          if (packet[6] == undefined) { flags = packet[5]; }
+          else { flags = packet[6].slice(1, -2); }
+  
+          var doc = {};
+          doc.timestamp = timestamp;
+          doc.protocol = 'IP';
+          doc.src_host = src.host;
+          doc.src_port = src.port;
+          doc.dst_host = dst.host;
+          doc.dst_port = dst.port;
+          doc.flags = flags;
+          doc.length = packet[packet.indexOf('length') + 1];
+          doc.nic = nic;
+  
+          packetsQueue[0].push(doc);
+        }
+        else if (packet[1] == 'ARP,') { /*console.log(packet);*/ }
+        else { /*console.log(packet);*/ }
+  
+        if (packetsQueue[0].length >= packetsLimit) {
+          flushPackets();
+        }
       }
     }
-  }
-});
+  });
+}
 
 function parseHostPort(str) {
   var str = str.split('.');
@@ -117,3 +122,7 @@ function elasticBulk(bulk) {
 
 var flushP = setInterval(flushPackets, interval);
 var flushB = setInterval(flushBulk, interval);
+
+for (var i=0; i<NICs.length; i++) {
+  spawns(NICs[i]);
+}
